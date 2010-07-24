d94a6c1674ca7a8fea19ddfb5bf5862c386f207b
Hadi Eskandari <h.eskandari@gmail.com>
2010-07-24 17:24:57 +0430

    -Removed superfulous checks -Used explicit namespaces when registering views

---
+++ Silverlight/Windsor.SLExample/Factories/CustomerFactory.cs
@@ -0,0 +1,32 @@
+using Castle.Core;
+using Castle.Windsor;
+using Windsor.SLExample.Model;
+
+namespace Windsor.SLExample.Factories
+{
+    public interface ICustomerFactory
+    {
+        Customer Create(string firstname, string lastname);
+    }
+
+    [Singleton]
+    public class CustomerFactory : ICustomerFactory
+    {
+        private readonly IWindsorContainer _container;
+
+        public CustomerFactory(IWindsorContainer container)
+        {
+            _container = container;
+        }
+
+        public Customer Create(string firstname, string lastname)
+        {
+            var c = _container.Resolve<Customer>();
+
+            c.Firstname = firstname;
+            c.Lastname = lastname;
+
+            return c;
+        }
+    }
+}
